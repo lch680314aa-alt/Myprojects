2ae35bd15632347a5c3826b17a3fd22ed91d2227
lch680314aa <lch680314aa@gmail.com>
2025-12-21 00:16:03 +0900

    fireworks success

--- src/main.js
+++ src/main.js
@@ -1,24 +1,38 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const canvas = document.getElementById('fireworksCanvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+class Particle {
+    constructor(x, y, color, velocityX, velocityY) {
+        this.x = x; this.y = y; this.color = color;
+        this.velocityX = velocityX; this.velocityY = velocityY;
+        this.alpha = 1; this.friction = 0.95; this.gravity = 0.2;
+    }
+    draw() {
+        ctx.save(); ctx.globalAlpha = this.alpha; ctx.beginPath();
+        ctx.arc(this.x, this.y, 2, 0, Math.PI * 2); ctx.fillStyle = this.color; ctx.fill(); ctx.restore();
+    }
+    update() {
+        this.velocityX *= this.friction; this.velocityY *= this.friction;
+        this.velocityY += this.gravity; this.x += this.velocityX; this.y += this.velocityY;
+        this.alpha -= 0.01;
+    }
+}
 
-setupCounter(document.querySelector('#counter'))
+let particles = [];
+function createFirework(x, y) {
+    const color = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    for (let i = 0; i < 100; i++) {
+        const velocity = Math.random() * 8 + 2; const angle = Math.random() * Math.PI * 2;
+        particles.push(new Particle(x, y, color, Math.cos(angle) * velocity, Math.sin(angle) * velocity));
+    }
+}
+
+function animate() {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.1)'; ctx.fillRect(0, 0, canvas.width, canvas.height);
+    particles.forEach((p, i) => { if (p.alpha > 0) { p.update(); p.draw(); } else { particles.splice(i, 1); }});
+    if (Math.random() < 0.05) createFirework(Math.random() * canvas.width, Math.random() * canvas.height * 0.5);
+    requestAnimationFrame(animate);
+}
+animate();
